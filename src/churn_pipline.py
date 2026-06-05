6cc8a766198cb6369a51eab305678bb7b7bdab51
Alihcyv <elihcyv@gmail.com>
2026-06-06 00:49:54 +0400

    Add churn pipeline for model evaluation

---
+++ src/churn_pipline.py
@@ -0,0 +1,87 @@
+%%writefile churn_pipline.py
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.metrics import roc_curve, roc_auc_score, f1_score, classification_report
+
+def preprocessing_churn(train_df: pd.DataFrame, test_df: pd.DataFrame) -> tuple:
+    print("*** Preprocessing Churn Dataset ***")
+    
+    train_df = train_df.copy()
+    test_df = test_df.copy()
+    
+    print(f"Class Imbalance:\n Train:\n{train_df['Churn'].value_counts()} \nTest:\n{test_df['Churn'].value_counts()}")
+    
+    num_columns = [col for col in train_df.columns 
+                   if pd.api.types.is_numeric_dtype(train_df[col]) and train_df[col].nunique() > 2]
+    
+    cat_cols = ['State', 'International plan', 'Voice mail plan']
+    ohe = OneHotEncoder(sparse_output=False, handle_unknown='ignore', drop='first')
+    
+    train_encoded = ohe.fit_transform(train_df[cat_cols])
+    test_encoded = ohe.transform(test_df[cat_cols])
+     
+    cols_names = ohe.get_feature_names_out(cat_cols)
+    train_ohe_df = pd.DataFrame(train_encoded, columns=cols_names, index=train_df.index)
+    test_ohe_df = pd.DataFrame(test_encoded, columns=cols_names, index=test_df.index)
+    
+    train_df = pd.concat([train_df, train_ohe_df], axis=1).drop(cat_cols, axis=1)
+    test_df = pd.concat([test_df, test_ohe_df], axis=1).drop(cat_cols, axis=1)
+    
+    le = LabelEncoder()
+    train_df['target'] = le.fit_transform(train_df['Churn'])
+    test_df['target'] = le.transform(test_df['Churn'])
+    
+    X_train = train_df.drop(['Churn', 'target'], axis=1)
+    y_train = train_df['target']
+    X_test = test_df.drop(['Churn', 'target'], axis=1)
+    y_test = test_df['target']
+
+    scaler = StandardScaler()
+    X_train[num_columns] = X_train[num_columns].astype(float)
+    X_test[num_columns] = X_test[num_columns].astype(float)
+    
+    X_train.loc[:, num_columns] = scaler.fit_transform(X_train[num_columns])
+    X_test.loc[:, num_columns] = scaler.transform(X_test[num_columns])
+    
+    return X_train, X_test, y_train, y_test, X_train.columns
+
+def evaluate_model(model, X_test, y_test, model_name):
+    print('\n', 20*"#", model_name, "#"*20, '\n')
+    
+    model.fit(X_train, y_train) 
+    y_pred = model.predict(X_test)
+    y_probs = model.predict_proba(X_test)[:, 1]
+    auc_score = roc_auc_score(y_test, y_probs)
+
+    print(f'F1-Score: {f1_score(y_test, y_pred):.4f}')
+    print(f"Roc-auc-score: {auc_score:.4f}")
+    print('\nClassification Report:\n', classification_report(y_test, y_pred))
+
+    fpr, tpr, _ = roc_curve(y_test, y_probs)
+    plt.figure(figsize=(6, 5))
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {auc_score:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'ROC-AUC Curve: {model_name}')
+    plt.legend(loc="lower right")
+    plt.grid(alpha=0.3)
+    plt.show()
+
+train_df = pd.read_csv("/kaggle/input/datasets/elihaciyev/ml-intern/churn-bigml-80.csv")
+test_df = pd.read_csv('/kaggle/input/datasets/elihaciyev/ml-intern/churn-bigml-20.csv')
+
+X_train, X_test, y_train, y_test, features = preprocessing_churn(train_df, test_df)
+
+models = [
+    (LogisticRegression(max_iter=500, C=10.0, class_weight='balanced', random_state=42), "Logistic Regression"),
+    (RandomForestClassifier(n_estimators=1000, max_depth=6, class_weight='balanced', random_state=42), "Random Forest Classifier"),
+    (SVC(class_weight='balanced', kernel='rbf', probability=True, random_state=42), "Support Vector Classifier")
+]
+
+for model_obj, name in models:
+    evaluate_model(model_obj, X_test, y_test, name)
